e502bf961fadeb1872269bef8929683044e88f14
nicomichelson <nicomichelson@gmail.com>
2022-02-04 15:59:06 -0300

    inicio

---
+++ src/Controller/Front/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_page")
+     */
+    public function index()
+    {
+        return $this->render('front/index.html.twig');
+    }
+}
